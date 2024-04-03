a8b8bc3a9003a90553e313f47d92bdd93bd4b08a
Andre-101 <kabh28@hotmail.com>
2024-04-03 13:02:32 -0500

    base

---
+++ src/main/java/com/example/backtms/BackTmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackTmsApplication.class, args);
+    }
+
+}
